d37a2494c221a639888dc1ce4ddf2a87b97e32dd
Arvin Nacional <arvin.nacionalzxc@gmail.com>
2024-07-01 00:08:21 +0800

    fixed variant props duplicate

--- components/ui/alert-dialog.tsx
+++ components/ui/alert-dialog.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
-import { VariantProps, VariantProps } from "class-variance-authority";
+import { VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
 
