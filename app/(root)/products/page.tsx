781ad8af000f2326971e3c0dfb8b29e68106ed16
Arvin Nacional <arvin.nacionalzxc@gmail.com>
2024-07-02 07:15:24 +0800

    fixed products and services page

--- app/(root)/products/page.tsx
+++ app/(root)/products/page.tsx
@@ -6,13 +6,9 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      {" "}
       <Header title="Products" img="/assets/images/about-image.jpg" />
-      <section className="flex items-center justify-center px-16  max-md:px-5">
-        <div className="w-[1200px] max-w-full flex justify-center pb-6 max-md:mt-10 flex-col items-center">
-          <HomeProducts />
-        </div>
-      </section>
+      <HomeProducts />
+
       <CTA
         title="Want to know more about our products?"
         action="Ask for a free consult"
